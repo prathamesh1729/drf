94e616d2e5f64003458ade6ab2eedce78380a892
Prathamesh <prathamesh1729@gmail.com>
2015-10-26 01:30:05 +0530

    Using routers to handle URLs automatically

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,9 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework import renderers
 from snippets.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework.routers import DefaultRouter
 
+#Create a router and register our viewsets with it
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+	url(r'^', include(router.urls)),
+]
+
+"""
 snippet_list = SnippetViewSet.as_view({
 	'get': 'list',
 	'post': 'create'
@@ -44,3 +55,4 @@ urlpatterns = [
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+"""
